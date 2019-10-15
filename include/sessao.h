6f853309f17c1db434172e64752defc50bb3dbcf
Your Name <you@example.com>
2019-10-15 11:31:20 -0300

    consertando makefile

--- include/sessao.h
+++ include/sessao.h
@@ -3,22 +3,21 @@
 #include <iostream>
 #include <map>
 #include <iterator>
+#include <string>
 #include "sala.h"
 #include "filme.h"
 
 
 class Sessao {
     private:
-    std::string datahora;
-    Filme filme;
-    Sala sala;
+        std::string datahora;
+        Filme filme;
+        Sala sala;
 
     public:
-    Sessao(Sala sala, Filme filme, std::string datahora):sala(sala), filme(filme), datahora(datahora){  
-        std::multimap <char,Assento> mapa_assentos;
-    }
-    ~Sessao();
-    void imprimir_mapa_assentos();
+        Sessao(Sala, Filme, std::string);
+        ~Sessao();
+        void imprimir_mapa_assentos();
 
 };
 #endif
